26b1b330ad6dec68be7f8aae89d1f6bab33a44d1
alxndr-w <github.com@alexplus.de>
2020-12-13 19:07:27 +0100

    update image, description, url fields

--- lib/neues_entry.php
+++ lib/neues_entry.php
@@ -1,12 +1,12 @@
 <?php
 
-use Ramsey\uuid\uuid;
-
 class neues_entry extends \rex_yform_manager_dataset
 {
-    private $startDate = null;
-    private $category = null;
-    private $offer = null;
+    private $publishdate;
+    private $category;
+    private $offer;
+    private $image = '';
+    private $url_label = '';
 
     public function getCategory()
     {
@@ -14,53 +14,76 @@ class neues_entry extends \rex_yform_manager_dataset
         return $this->category;
     }
 
-    public function getTimezone($lat, $lng){
-        $neues_timezone = "https://maps.googleapis.com/maps/api/timezone/json?location=" . $lat . "," . $lng . "&timestamp=" . time() . "&sensor=false";
+    public function getTimezone($lat, $lng)
+    {
+        $neues_timezone = 'https://maps.googleapis.com/maps/api/timezone/json?location=' . $lat . ',' . $lng . '&timestamp=' . time() . '&sensor=false';
         $neues_location_time_json = file_get_contents($neues_timezone);
         return $neues_location_time_json;
     }
 
-    public function getImage() :string
+    public function getImage(): string
     {
+        if ('' == $this->image) {
+            $this->image = rex_config::get('neues', 'default_thumbnail');
+        }
         return $this->image;
     }
+
     public function getMedia()
     {
         return rex_media::get($this->image);
     }
 
-    public function getDescriptionAsPlaintext() :string
+    public function getDescriptionAsPlaintext(): string
     {
         return strip_tags($this->description);
     }
 
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    public function getUrlLabel(): string
+    {
+        if ('' == $this->url_label) {
+            $this->url_label = rex_config::get('neues', 'default_url_label');
+        }
+        return $this->url_label;
+    }
+
     public function getUid()
     {
-        if ($this->uid === "" && $this->getValue("uid") === "") {
+        if ('' === $this->uid && '' === $this->getValue('uid')) {
             $this->uid = self::generateUuid($this->id);
 
-            rex_sql::factory()->setQuery("UPDATE rex_neues_entry SET uid = :uid WHERE id = :id", [":uid"=>$this->uid, ":id" => $this->getId()]);
+            rex_sql::factory()->setQuery('UPDATE rex_neues_entry SET uid = :uid WHERE id = :id', [':uid' => $this->uid, ':id' => $this->getId()]);
         }
         return $this->uid;
     }
 
-    private function getDateTime($date = null, $time = "00:00")
+    private function getDateTime($date = null, $time = '00:00')
     {
-        $time = explode(":", $time);
+        $time = explode(':', $time);
         $dateTime = new DateTime($date);
         $dateTime->setTime($time[0], $time[1]);
 
         return $dateTime;
     }
 
-    public function getStartDate()
+    public function getPublishDate()
     {
-        $this->startDate = $this->getDateTime($this->getValue("startDate"), $this->getValue("startTime"));
-        return $this->startDate;
+        $this->publishdate = $this->getDateTime($this->getValue('publishdate'));
+        return $this->publishdate;
     }
 
     public function getName()
     {
-        return $this->getValue("name");
+        return $this->getValue('name');
     }
 }
--- pages/neues.settings.php
+++ pages/neues.settings.php
@@ -3,14 +3,15 @@
 $addon = rex_addon::get('neues');
 
 $form = rex_config_form::factory($addon->name);
-/*
-$field = $form->addInputField('text', 'currency', null, ["class" => "form-control"]);
-$field->setLabel(rex_i18n::msg('neues_currency'));
 
-$field = $form->addInputField('text', 'timezone_api_code', null, ["class" => "form-control"]);
-$field->setLabel(rex_i18n::msg('neues_timezone'));
-$field->setNotice(rex_i18n::msg('neues_timezone_notice'). '<a href="https://developers.google.com/maps/documentation/timezone/intro?hl=de">https://developers.google.com/maps/documentation/timezone/intro?hl=de</a>');
-*/
+$field = $form->addMediaField('default_thumbnail');
+$field->setPreview(1);
+$field->setTypes('jpg,gif,png');
+$field->setLabel('Vorschau-Bild');
+
+$field = $form->addInputField('text', 'default_url_label', null, ['class' => 'form-control']);
+$field->setLabel(rex_i18n::msg('default_url_label'));
+
 $fragment = new rex_fragment();
 $fragment->setVar('class', 'edit', false);
 $fragment->setVar('title', $addon->i18n('neues_settings'), false);
