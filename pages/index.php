a9d5a3befd86e331aa5c1acf6abdcbe831697496
Alexander Walther <aw@alexplus.de>
2022-12-26 21:59:16 +0100

    rexstan fix $this

--- pages/index.php
+++ pages/index.php
@@ -1,4 +1,4 @@
 <?php
 
-echo rex_view::title($this->getProperty('page')['title']);
+echo rex_view::title(rex_addon::get('neues')->getProperty('page')['title']);
 rex_be_controller::includeCurrentPageSubPath();
