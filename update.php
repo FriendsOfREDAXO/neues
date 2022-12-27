35f8f3b1098165de9c55a1a371b915cd55c7de3a
Alexander Walther <aw@alexplus.de>
2022-12-27 11:52:13 +0100

    load new tableset on update

---
+++ update.php
@@ -0,0 +1,6 @@
+<?php
+
+/* Tablesets aktualisieren */
+
+
+rex_yform_manager_table_api::importTablesets(rex_file::get(rex_path::addon(rex_addon::get("neues")->getName(), 'install/tableset.json')));
