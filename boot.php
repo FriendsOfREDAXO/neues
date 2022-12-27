a25e2114fce98fa65f37ea5e5a6a570c6b11b4a2
Alexander Walther <aw@alexplus.de>
2022-12-27 14:44:43 +0100

    non-BC Updates
    * Übearbeitete Tablesets-Struktur
    * neue Domainauswahl (benötigt yform_field)
    * neue Auswahl für Veröffentlichungsdatum (benötigt yform_field)
    * neue Editor-Auswahl

--- boot.php
+++ boot.php
@@ -9,11 +9,20 @@ rex_yform_manager_dataset::setModelClass(
     neues_category::class
 );
 
+if (rex::isBackend() && rex_be_controller::getCurrentPage() == "neues/entry" || rex_be_controller::getCurrentPage() == "yform/manager/data_edit") {
+    rex_extension::register('OUTPUT_FILTER', function (rex_extension_point $ep) {
+        $suchmuster = 'class="###neues-settings-editor###"';
+        $ersetzen = rex_config::get("neues", "editor");
+        $ep->setSubject(str_replace($suchmuster, $ersetzen, $ep->getSubject()));
+    });
+}
+
+
 if (rex_plugin::get('yform', 'rest')->isAvailable() && !rex::isSafeMode()) {
     /* YForm Rest API */
     $rex_neues_entry_route = new \rex_yform_rest_route(
         [
-            'path' => '/v0.dev/neues/date/',
+            'path' => '/neues/3/date/',
             'auth' => '\rex_yform_rest_auth_token::checkToken',
             'type' => \neues_entry::class,
             'query' => \neues_entry::query(),
@@ -87,3 +96,56 @@ if (rex_plugin::get('yform', 'rest')->isAvailable() && !rex::isSafeMode()) {
 
     \rex_yform_rest::addRoute($rex_neues_category_route);
 }
+
+
+
+rex_extension::register('YFORM_DATA_LIST', function ($ep) {
+    if ($ep->getParam('table')->getTableName()=="rex_neues_entry") {
+        $list = $ep->getSubject();
+
+        $list->setColumnFormat(
+            'name',
+            'custom',
+            function ($a) {
+                $_csrf_key = rex_yform_manager_table::get('rex_neues_entry')->getCSRFKey();
+                $token = rex_csrf_token::factory($_csrf_key)->getUrlParams();
+
+                $params = array();
+                $params['table_name'] = 'rex_neues_entry';
+                $params['rex_yform_manager_popup'] = '0';
+                $params['_csrf_token'] = $token['_csrf_token'];
+                $params['data_id'] = $a['list']->getValue('id');
+                $params['func'] = 'edit';
+    
+                return '<a href="'.rex_url::backendPage('neues/entry', $params) .'">'. $a['value'].'</a>';
+            }
+        );
+        $list->setColumnFormat(
+            'event_category_id',
+            'custom',
+            function ($a) {
+                $_csrf_key = rex_yform_manager_table::get('rex_neues_category')->getCSRFKey();
+                $token = rex_csrf_token::factory($_csrf_key)->getUrlParams();
+
+                $params = array();
+                $params['table_name'] = 'rex_neues_category';
+                $params['rex_yform_manager_popup'] = '0';
+                $params['_csrf_token'] = $token['_csrf_token'];
+                $params['data_id'] = $a['list']->getValue('id');
+                $params['func'] = 'edit';
+    
+                $return = [];
+
+                $category_ids = array_filter(explode(",", $a['value']));
+
+                foreach ($category_ids as $category_id) {
+                    $event = event_category::get($category_id);
+                    if ($event) {
+                        $return[] = '<a href="'.rex_url::backendPage('neues/category', $params) .'">'. $event->getName().'</a>';
+                    }
+                }
+                return implode("<br>", $return);
+            }
+        );
+    }
+});
--- pages/neues.settings.php
+++ pages/neues.settings.php
@@ -9,11 +9,15 @@ $field->setPreview(1);
 $field->setTypes('jpg,gif,png');
 $field->setLabel(rex_i18n::msg('neues_default_thumbnail'));
 
-$field = $form->addInputField('text', 'default_url_label', null, ['class' => 'form-control']);
-$field->setLabel(rex_i18n::msg('neues_default_url_label'));
+$field = $form->addInputField('text', 'external_url_label', null, ['class' => 'form-control']);
+$field->setLabel(rex_i18n::msg('neues_external_url_label'));
 
 $field = $form->addInputField('text', 'default_author', null, ['class' => 'form-control']);
 $field->setLabel(rex_i18n::msg('neues_default_author'));
+
+$field = $form->addInputField('text', 'editor', null, ['class' => 'form-control']);
+$field->setLabel(rex_i18n::msg('neues_editor'));
+$field->setNotice('z.B. <code>class="form-control redactor-editor--default"</code>');
 
 $fragment = new rex_fragment();
 $fragment->setVar('class', 'edit', false);
