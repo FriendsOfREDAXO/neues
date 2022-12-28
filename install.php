41d19344779b03296e59dc22f9de14ad783af550
Alexander Walther <aw@alexplus.de>
2022-12-28 10:40:04 +0100

    fix update.php

--- install.php
+++ install.php
@@ -1,6 +1,4 @@
 <?php
 
 /* Tablesets aktualisieren */
-
-
 rex_yform_manager_table_api::importTablesets(rex_file::get(rex_path::addon(rex_addon::get("neues")->getName(), 'install/tableset.json')));
