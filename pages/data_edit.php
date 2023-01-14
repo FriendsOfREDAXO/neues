1d1f51e8d798db819e19c5c9bb5f66527827c232
Alexander Walther <aw@alexplus.de>
2023-01-15 00:42:53 +0100

    Whoops bei Rollen vermeiden, neue data_edit

--- pages/data_edit.php
+++ pages/data_edit.php
@@ -1,63 +1,33 @@
 <?php
-$target_page = rex_request('page', 'string');
-if ($target_page == 'yform/manager/data_edit') {
-    $table_name = rex_request('table_name', 'string');
-    $wrapper = '';
-    $show_title = true;
-} elseif (isset(rex_package::get("neues")->getProperty('pages')[rex_be_controller::getCurrentPagePart(1)])) {
-    # page-Properties allgemein abrufen
-    $properties = rex_package::get("neues")->getProperty('pages')[rex_be_controller::getCurrentPagePart(1)];
-    if ($sub=rex_be_controller::getCurrentPagePart(2)) {
-        $properties = $properties['subpages'][$sub];
-    }
-    # yform-properties
-    $table_name = isset($properties['yformTable']) ? $properties['yformTable'] : '';
-    $wrapper = isset($properties['yformClass']) ? $properties['yformClass'] : '';
-    $show_title = $properties['yformTitle'] ?? false;
-} else {
-    $table_name = '';
+$yform = rex_package::get("neues")->getProperty('pages')[rex_be_controller::getCurrentPagePart(1)]['subpages'][rex_be_controller::getCurrentPagePart(2)]['yform'];
+
+$table_name = $yform['table_name'] ?? '';
+$show_title = true === ($yform['show_title'] ?? false);
+$wrapper_class = $yform['wrapper_class'] ?? '';
+
+if ($table_name) {
+    $_REQUEST['table_name'] = $table_name;
 }
 
-$table = rex_yform_manager_table::get($table_name);
-
-if ($table && rex::getUser() && (rex::getUser()->isAdmin() || rex::getUser()->getComplexPerm('yform_manager_table')->hasPerm($table->getTableName()))) {
-    try {
-        $page = new rex_yform_manager();
-        $page->setTable($table);
-        $page->setLinkVars(['page' => $target_page, 'table_name' => $table->getTableName()]);
-
-        if ($wrapper) {
-            echo "<div class=\"$wrapper\">";
-        }
-
-        if ($show_title) {
-            echo $page->getDataPage();
-        } else {
-            # Seite erzeugen und abfangen
-            ob_start();
-            echo $page->getDataPage();
-            $page = ob_get_clean();
-            # Such den Header - Fall 1: mit Suchspalte?
-            $p = strpos($page, '<div class="row">');
-            # Such den Header - Fall 2: ohne Suchspalte
-            if ($p === false) {
-                $p = strpos($page, '<section class="rex-page-section">');
+if (!$show_title) {
+    \rex_extension::register(
+        'YFORM_MANAGER_DATA_PAGE_HEADER',
+        function (\rex_extension_point $ep) {
+            if ($ep->getParam('yform')->table->getTableName() === $ep->getParam('table_name')) {
+                return '';
             }
-            # Header rauswerfen
-            if ($p !== false) {
-                $page = ''.substr($page, $p);
-            }
-            # ausgabe
-            echo $page;
-        }
+        },
+        \rex_extension::EARLY,
+        ['table_name'=>$table_name]
+    );
+}
 
-        if ($wrapper) {
-            echo '</div>';
-        }
-    } catch (Exception $e) {
-        $message = nl2br($e->getMessage()."\n".$e->getTraceAsString());
-        echo rex_view::warning($message);
-    }
-} elseif (!$table) {
-    echo rex_view::warning(rex_i18n::msg('yform_table_not_found'));
+if ($wrapper_class) {
+    echo '<div class="',$wrapper_class,'">';
+}
+
+include \rex_path::plugin('yform', 'manager', 'pages/data_edit.php');
+
+if ($wrapper_class) {
+    echo '</div>';
 }
