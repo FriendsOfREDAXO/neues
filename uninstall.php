5ea92810fe8c2fe80cca0366b7f04d4aeb1204ff
Alexander Walther <github.com@alexplus.de>
2023-03-25 09:35:00 +0100

    Create uninstall.php

---
+++ uninstall.php
@@ -0,0 +1,10 @@
+<?php
+
+rex_config::removeNamespace("neues");
+if (rex_addon::get('yform')->isAvailable() && !rex::isSafeMode()) {
+    rex_yform_manager_table_api::removeTable('rex_neues_category');
+    rex_yform_manager_table_api::removeTable('rex_neues_entry');
+    rex_yform_manager_table_api::removeTable('rex_neues_entry_lang');
+    rex_yform_manager_table_api::removeTable('rex_neues_entry_category_rel');
+}
+
