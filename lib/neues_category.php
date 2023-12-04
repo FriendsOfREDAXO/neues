dd744ab88b001f4c8b0bad0483dbb07042542b40
Paul Götz <20090597+schorschy@users.noreply.github.com>
2023-12-04 12:42:17 +0100

    Update neues_category.php

--- lib/neues_category.php
+++ lib/neues_category.php
@@ -7,10 +7,20 @@ class neues_category extends \rex_yform_manager_dataset
     {
         return $this->getValue('name');
     }
+    public function setName(string $string) :self
+    {
+        $this->setValue('name', $string);
+        return $this;
+    }
 
     /** @api */
     public function getEntries(): ?rex_yform_manager_collection
     {
         return $this->getRelatedDataset('entry_ids');
+    }
+    public function setEntries(rex_yform_manager_collection $entries): self
+    {
+        $this->setValue('entry_ids', $entries);
+        return $this;
     }
 }
