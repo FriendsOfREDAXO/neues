d9f945a7464ba28d33e3f94f72406bcf61135074
Alexander Walther <mail@alexplus.de>
2023-12-17 14:26:03 +0100

    Cronjob automatisch installieren und aktivieren

--- install.php
+++ install.php
@@ -16,3 +16,10 @@ if(!rex_media::get('neues_entry_fallback_image.png')) {
 
     rex_media_service::addMedia($data, false);
 }
+
+/* Cronjob installieren */
+$cronjob = array_filter(rex_sql::factory()->getArray("SELECT * FROM rex_category WHERE `type` = 'rex_cronjob_neues_publish'"));
+if(!$cronjob) {
+    $query = rex_file::get(rex_path::addon('neues', 'install/rex_cronjob_neues_publish.sql'));
+    rex_sql::factory()->setQuery($query);
+}
