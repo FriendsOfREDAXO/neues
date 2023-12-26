79a5be8f5abff9eda1e5967047e19cf2adcc892b
Alexander Walther <mail@alexplus.de>
2023-12-26 15:35:49 +0100

    hotfix register cronjob

--- boot.php
+++ boot.php
@@ -1,5 +1,9 @@
 <?php
 
+if(rex_addon::get('cronjob')->isAvailable() && !rex::isSafeMode()) {
+    rex_cronjob_manager::registerType('rex_cronjob_neues_publish');
+}
+
 if (rex_addon::get('yform')->isAvailable() && !rex::isSafeMode()) {
 
     rex_yform_manager_dataset::setModelClass(
