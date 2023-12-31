2b8227d4bfa077b5ada84c5b79ccb2049c226087
Alexander Walther <mail@alexplus.de>
2023-12-31 16:04:31 +0100

    tiny fixes

--- uninstall.php
+++ uninstall.php
@@ -4,6 +4,7 @@ rex_config::removeNamespace('neues');
 if (rex_addon::get('yform')->isAvailable() && !rex::isSafeMode()) {
     rex_yform_manager_table_api::removeTable('rex_neues_category');
     rex_yform_manager_table_api::removeTable('rex_neues_entry');
-    rex_yform_manager_table_api::removeTable('rex_neues_entry_lang');
     rex_yform_manager_table_api::removeTable('rex_neues_entry_category_rel');
+    rex_yform_manager_table_api::removeTable('rex_neues_author');
+    rex_yform_manager_table_api::removeTable('rex_neues_entry_lang');
 }
